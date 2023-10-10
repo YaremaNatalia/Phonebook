0a5703583cf715f6e0a8d3accb1af6f0d2204743
YaremaNatalia <yaremka.n@gmail.com>
2023-10-10 23:39:09 +0300

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { Suspense, lazy, useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectAuthentificated, selectUserLoading } from 'redux/selectors';
+import { selectUserLoading } from 'redux/selectors';
 
 import { refreshUserThunk } from 'redux/authentifServices';
 
 import { Loader } from './components/Loader';
-import { AppNav, Container } from 'App.styled';
+import { Container } from 'App.styled';
 import PrivateRoute from 'components/PrivateRoute/PrivateRoute';
-import UserMenu from 'components/UserMenu/UserMenu';
-import { Navigation } from 'components/Navigation/Navigation';
-import { AuthentifNav } from 'components/AuthentifNav/AuthentifNav';
 import PublicRoute from 'components/PublicRoute/PublicRoute';
+import { AppBar } from 'components/AppBar/AppBar';
+// import { Layout } from 'Layout';
 
 const HomePage = lazy(() => import('pages/HomePage'));
 const LoginPage = lazy(() => import('pages/LoginPage'));
@@ -20,7 +19,6 @@ const ContactsPage = lazy(() => import('pages/ContactsPage'));
 
 export const App = () => {
   const dispatch = useDispatch();
-  const authentificated = useSelector(selectAuthentificated);
   const isLoading = useSelector(selectUserLoading);
 
   useEffect(() => {
@@ -31,45 +29,38 @@ export const App = () => {
     <Container>
       {!isLoading && (
         <>
-          <header>
-            <AppNav>
-              <Navigation />
-              {authentificated ? <UserMenu /> : <AuthentifNav />}
-            </AppNav>
-          </header>
-          <main>
-            <Suspense fallback={<Loader />}>
-              <Routes>
-                {/* <Route path="/" element={<Navigation />} /> */}
-                <Route index element={<HomePage />} />
-                <Route
-                  path="/register"
-                  element={
-                    <PublicRoute redirectTo="/contacts">
-                      <RegisterPage />
-                    </PublicRoute>
-                  }
-                />
-                <Route
-                  path="/login"
-                  element={
-                    <PublicRoute redirectTo="/contacts">
-                      <LoginPage />
-                    </PublicRoute>
-                  }
-                />
-                <Route
-                  path="/contacts"
-                  element={
-                    <PrivateRoute redirectTo="/login">
-                      <ContactsPage />
-                    </PrivateRoute>
-                  }
-                />
-                <Route path="*" element={<Navigate to="/" />} />
-              </Routes>
-            </Suspense>
-          </main>
+          <AppBar />
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              {/* <Route path="/" element={<Layout />} /> */}
+              <Route index element={<HomePage />} />
+              <Route
+                path="/register"
+                element={
+                  <PublicRoute redirectTo="/contacts">
+                    <RegisterPage />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  <PublicRoute redirectTo="/contacts">
+                    <LoginPage />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="/contacts"
+                element={
+                  <PrivateRoute redirectTo="/login">
+                    <ContactsPage />
+                  </PrivateRoute>
+                }
+              />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </Suspense>
         </>
       )}
     </Container>
